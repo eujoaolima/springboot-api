71c0643f7aa51046266283d86aa6ee5e9aac51fa
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-08-18 16:35:54 -0300

    Criação da entidade Product, seu serviço, seu controlador e seu repositório

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -38,13 +43,20 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2021-08-18T18:04:55Z"), OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2022-09-18T19:05:56Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+		Product p1 = new Product(null, "Batata", "Pacote de batatas", 10.00, "batata.png");
+		Product p2 = new Product(null, "S23+ 512gb", "Celular brabo", 4500.00, "batata.png");
+		Product p3 = new Product(null, "Carrinho de controle remoto", "Carrinho de controle remoto", 249.97, "batata.png");
+
 		Category c1 = new Category(null, "Brinquedos");
 		Category c2 = new Category(null, "Comidas");
 		Category c3 = new Category(null, "Eletrônicos");
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
